c4d2e672b991d24b1e06e439381eec44e6a3a38c
Joao Victor <jvasprogramando@gmail.com>
2025-04-17 15:52:07 -0300

    1007 - Difference

---
+++ beginner/bee1007.py
@@ -0,0 +1,11 @@
+def difference(a, b, c, d):
+    diff = (a * b - c * d)
+    return diff
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(f'DIFERENCA = {difference(a, b, c, d)}')
