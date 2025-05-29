b6907deb92a3076854ee2511ee50da6e5a151c5a
Joao Victor <jvasprogramando@gmail.com>
2025-05-29 08:53:53 -0300

    Bee 1038 ainda não finalizado

---
+++ beginner/bee1038.py
@@ -0,0 +1,15 @@
+order = list()
+
+specification_dic = {
+    '1': 4.00,
+    '2': 4.50,
+    '3': 5.00,
+    '4': 2.00,
+    '5': 1.50
+}
+
+def snack(list_order):
+
+
+order = input().split()
+snack(order)
