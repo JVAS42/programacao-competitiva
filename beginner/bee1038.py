1ecc6c8dcd0710c915d56d8c92cba5aa3fca6ae4
Joao Victor <jvasprogramando@gmail.com>
2025-06-02 07:48:09 -0300

    Finished Bee 1040 - Snack

--- beginner/bee1038.py
+++ beginner/bee1038.py
@@ -1,15 +1,14 @@
-order = list()
-
-specification_dic = {
+specification = {
     '1': 4.00,
     '2': 4.50,
     '3': 5.00,
     '4': 2.00,
     '5': 1.50
 }
+def snack(cod, quant):
+    total = (specification[cod] * quant)
+    return total
 
-def snack(list_order):
-
-
-order = input().split()
-snack(order)
+cod, quant = input().split()
+quant = int(quant)
+print(f'Total: R$ {snack(cod, quant):.2f}')
