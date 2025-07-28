8124220ecc4930b833c477205ccf89e1616d5c61
João Victor <61116930+JVAS42@users.noreply.github.com>
2025-07-28 16:30:03 -0300

    Update bee1041.py

--- beginner/bee1041.py
+++ beginner/bee1041.py
@@ -8,9 +8,9 @@ def coordinate(cord):
     elif cord[0] > 0 and cord[1] < 0:
         print('Q4')
     elif cord[0] == 0 and cord[1] != 0:
-        print('Eixo X')
-    elif cord[1] == 0 and cord[0] !=0:
         print('Eixo Y')
+    elif cord[1] == 0 and cord[0] !=0:
+        print('Eixo X')
     else:
         print('Origem')
 
