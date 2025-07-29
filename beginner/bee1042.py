ccf2f8f7c9f10b1b1fa56f738c8d6e815e389a95
JVAS42 <jvasprogramando@gmail.com>
2025-07-29 13:40:27 -0300

    Update bee1042

--- beginner/bee1042.py
+++ beginner/bee1042.py
@@ -1,10 +1,10 @@
 def sort_simples(values):
     ordered_value = sorted(values)
     for i in range(0, len(ordered_value)):
-        print(ordered_value[i])
+        print(f'{ordered_value[i]:.0f}')
     print()
     for i in range(0, len(values)):
-        print(values[i])
+        print(f'{values[i]:.0f}')
 
 value_list = input().split()
 values = list(map(float, value_list))
