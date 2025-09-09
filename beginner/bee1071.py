b0856e3ffba31bd1e1f5d6cfafc7b11d9239e1c0
JVAS42 <jvasprogramando@gmail.com>
2025-09-09 15:47:07 -0300

    Bee 1071 update

--- beginner/bee1071.py
+++ beginner/bee1071.py
@@ -1,19 +1,13 @@
-interval = list()
-
-def sum_odd(interval):
-    sum = 0
-    interval.sort()
-    for i in range(interval[0], interval[1]):
-        if i % 2 != 0:
-            if i < 0:
-                sum -= i * (-1)
-            else:
-                sum += i
-    
-    print(sum)
-
 x = int(input())
-interval.append(x)
 y = int(input())
-interval.append(y)
-sum_odd(interval)
+
+soma = 0
+
+if x > y:
+    x, y = y, x
+
+for i in range(x + 1, y):
+    if i % 2 != 0:
+        soma += i
+
+print(soma)
