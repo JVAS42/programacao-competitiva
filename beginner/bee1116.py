222b07cc5641f36c41a6fb97c16bf19b4fdcaaaa
JVAS42 <jvasprogramando@gmail.com>
2025-10-14 18:15:49 -0300

    Iniciando o exercicio 1116

---
+++ beginner/bee1116.py
@@ -0,0 +1,9 @@
+operators = list()
+amount = int(input())
+
+for i in range(0, amount):
+    values = input().split()
+    values = list(map(float, values))
+
+    operators.append[:]
+    values.clear()
