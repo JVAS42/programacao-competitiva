8990f4f94598c2bd8e26d003d89e69c045ffc6e7
JVAS42 <jvasprogramando@gmail.com>
2025-10-15 10:47:18 -0300

    Bee 1118 - Several Scores with Validation incomplete

---
+++ beginner/bee1118.py
@@ -0,0 +1,25 @@
+def average():
+    average = 0
+    cont = 0
+    while cont < 2:
+        note = float(input())
+        if note >= 0 and note <= 10:
+            average += note
+            cont += 1
+        else:
+            print('nota invalida')
+    print(f'media = {average/2:.2f}')
+
+def menu():
+    while True:
+        opcion = int(input('novo calculo (1-sim 2-nao)'))
+
+        if opcion == 1:
+            average()
+        elif opcion == 2:
+            exit()
+        else:
+            continue
+
+average()
+menu()
