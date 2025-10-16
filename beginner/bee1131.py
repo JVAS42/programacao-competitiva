33cb958565661808c68b689cd5ca0d7d81fb3c3f
JVAS42 <jvasprogramando@gmail.com>
2025-10-16 15:19:28 -0300

    Bee 1131 - Grenais resolved

--- beginner/bee1131.py
+++ beginner/bee1131.py
@@ -1,23 +1,53 @@
-placares = list()
+leaderboards = list()
 
 def data():
-    placar = input().split()
-    placar = list(map(int, placar))
+    scoreboard = input().split()
+    scoreboard = list(map(int, scoreboard))
 
-    placares.append(placar[:])
-    placar.clear()
+    leaderboards.append(scoreboard[:])
+    scoreboard.clear()
+
+
+def calculate():
+    total = 0
+    inter = 0
+    gremio = 0
+    draw = 0
+
+    for i in range(0, len(leaderboards)):
+        if leaderboards[i][0] > leaderboards[i][1]:
+            inter += 1
+        elif leaderboards[i][0] < leaderboards[i][1]:
+            gremio += 1
+        else:
+            draw += 1
+        total += 1
+    
+    print(f'{total} grenais')
+    print(f'Inter:{inter}')
+    print(f'Gremio:{gremio}')
+    print(f'Empates:{draw}')
+    if inter > gremio:
+        print(f'Inter venceu mais')
+    elif inter < gremio:
+        print(f'Gremio venceu mais')
+    else:
+        print('Nao houve vencedor')
+
 
 def menu():
     data()
     while True:
-        opcion = int(input('Novo grenal (1-sim 2-nao)'))
+        print('Novo grenal (1-sim 2-nao)')
+        opcion = int(input())
 
         if opcion == 1:
             data()
         elif opcion == 2:
-            print('Placares: ', placares)
+            calculate()
             break
         else:
             continue
+
 
 menu()
